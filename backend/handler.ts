a472204769a691b0fa0b47dba0f50a868ad8ec28
djaysax <79742664+cemmey@users.noreply.github.com>
2022-11-15 21:27:18 +0100

    Implemented TODO in auth0Authorizer file

---
+++ backend/handler.ts
@@ -0,0 +1,15 @@
+'user strict'
+
+module.exports.hello = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfull!',
+        input: event
+      },
+      null,
+      2
+    )
+  }
+}
